a51fe4314b5f5125a55093b4ebb93bc223df8c2f
Ana Karen Millán López <159275080+AnaKMLopez@users.noreply.github.com>
2025-03-04 17:55:01 +0000

    04-03

---
+++ Scripts/2024/03/12/crea.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE escueltia;
+
+GO
+
+use escuelita
+
+CREATE TABLE empleado (
+    idEmpleado INT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    sueldo money DEFAULT 1500
+)
+
+INSERT INTO empleado (idEmpleado, nombre, sueldo)
+    VALUES (1, 'Felipde de Jesus', 2000)
+
+SELECT * FROM empleado;
+
+INSERT INTO empleado (idEmpleado, nombre)
+    VALUES (2, 'Chuerk')
+
+SELECT * FROM empleado
+
+CREATE TABLE personita (
+    idEmpleado INT NOT NULL PRIMARY KEY,
+    nombre NVARCHAR (50) NOT NULL,
+    sueldo money DEFAULT 1500,
+    telefono VARCHAR (20) CHECK (telefono LIKE '667[0-9][0-9]')
+)
+
+SELECT * FROM personita
+
+INSERT INTO personita VALUES (1, 'Juana', NULL, '667123')
+
+ALTER TABLE empleado    
+    ADD sexo CHAR(1) NULL CHECK (sexo in ('H', 'M'))
+
+SELECT * FROM empleado
+
+INSERT INTO empleado (idEmpleado, nombre, sexo) VALUES (3, 'Alexis Jara Rodriguez', 'M')
+
+INSERT INTO empleado (idEmpleado, nombre, sexo) VALUES (4, 'Matilde Sandoval', 'F')
+
+ALTER TABLE empleado
+    DROP CONSTRAINT CK__empleado__sexo__1AD3FDA4
+
+INSERT INTO empleado (idEmpleado, nombre, sexo) VALUES (4, 'Matilde Sandoval', 'F')
+
+SELECT * FROM empleado
+
+ALTER TABLE empleado
+    ADD CONSTRAINT sexonuevasopciones CHECK (sexo in ('H', 'M', 'T', 'F'))
+
+INSERT INTO empleado (idEmpleado, nombre, sexo) VALUES (5, 'Refugio Saldivar', 'F')
+
+INSERT INTO empleado (idEmpleado, nombre, sexo) VALUES (6, 'Delia Barrante', 'R')
+
+SELECT getdate()
+
+ALTER TABLE empleado
+    ADD nacimiento date not null check (date < getDate())
