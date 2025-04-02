40922c84f3417942c077010f68e79b3161647e93
Ana Karen Millán López <159275080+AnaKMLopez@users.noreply.github.com>
2025-04-02 17:51:58 +0000

    02-04-25

--- Scripts/2024/03/12/consultas-red-social.sql
+++ Scripts/2024/03/12/consultas-red-social.sql
@@ -14,3 +14,13 @@ SELECT * FROM Highschooler H1
     JOIN LIKES L ON L.ID1 = H1.ID
     JOIN Highschooler H2 ON L.ID2 = H2.ID
     WHERE H1.grade -2 >= H2.grade
+
+
+--PERSONAS QUE SE GUSTAN MUTUAMENTE
+SELECT H1.NAME, H1.grade, H2.name, H2.grade FROM Likes L1
+    JOIN LIKES L2 ON L1.ID2 = L2.ID1
+    JOIN Highschooler H1 ON H1.ID = L1.ID1
+    JOIN Highschooler H2 ON H2.ID = L1.ID2
+    WHERE L1.ID1 = L2.ID2
+
+--cada situacion donde el estudiante A le gusta un esstudiante B, pero el estudiante B le gusta el C, dar los 3 nombres
