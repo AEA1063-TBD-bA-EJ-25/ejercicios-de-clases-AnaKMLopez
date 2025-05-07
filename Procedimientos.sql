c4a490fd25d12e4e36ec1a227142a7885d4c2822
Ana Karen Millán López <159275080+AnaKMLopez@users.noreply.github.com>
2025-05-07 17:52:00 +0000

    07-05-25

---
+++ Procedimientos.sql
@@ -0,0 +1,18 @@
+USE Northwind
+
+GO
+
+CREATE PROCEDURE dbo.mathtutor
+    @m1 SMALLINT,
+    @m2 SMALLINT,
+    @result SMALLINT OUTPUT
+    AS
+        SET @result = @m1 * @m2
+
+GO
+
+DECLARE @answer SMALLINT
+DECLARE @f SMALLINT
+SET @f = 8
+EXECUTE mathtutor @f, 10, @answer OUTPUT
+SELECT 'The result is: ', @answer
